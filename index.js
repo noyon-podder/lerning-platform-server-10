ccb0f4619010a8f62b2e456a8513f90fde958e40
noyon-podder <noyon.vai369@gmail.com>
2022-10-26 01:49:40 +0600

    course details data link path is ready this commit

--- index.js
+++ index.js
@@ -9,6 +9,17 @@ app.get('/courses', (req, res) => {
     res.send(courses);
 })
 
+const courseDetails = require('./data/courseDetails.json');
+
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const getSingleCourseDetails = courseDetails.find(course => course.id === id);
+    if(!getSingleCourseDetails){
+        res.send({})
+    }
+    res.send(getSingleCourseDetails);
+})
+
 app.listen(Port, () => {
     console.log('server is running ', Port);
 })
